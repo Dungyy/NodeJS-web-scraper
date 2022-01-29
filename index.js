d3a5353e8587f51bec93d10a8ca05bb244c9d101
Dungyy <erickmunoz13@gmail.com>
2022-01-28 23:17:02 -0600

    scraped web for moniter's and created a txt file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 
 const start = async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto("https://learnwebcode.github.io/practice-requests/")
-    await page.screenshot({ path: "amazing.png" });
-    await browser.close()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com/search?q=programming+monitors&sxsrf=APq-WBsr0ELOOi3dO9CJ-me7nOaLmRyLZQ:1643432834313&source=lnms&tbm=shop&sa=X&ved=2ahUKEwjllrSymNb1AhVBKDQIHdVbDfoQ_AUoAXoECAEQAw&biw=856&bih=746&dpr=1.25");
+
+  const names = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".i0X6df")).map(
+      (x) => x.textContent
+    );
+  });
+  await fs.writeFile("monitors.txt", names.join("\r\n"));
+
+  await browser.close();
 };
 
-start()
+start();
